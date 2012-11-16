f682c2a9186ae1fae632c1170e689d011f8df259
Laurent Zuijdwijk <laurent.zuijdwijk@gmail.com>
2012-11-16 23:19:00 +0000

    Added tests and updated readme

--- README.md
+++ README.md
@@ -5,7 +5,42 @@ A complete cross-browser form validation script, either stand-alone, or as a JQu
 
 I aimed to make an easy to use form validation plugin, which will work in any browser and any circumstance. It is easy to use and will conform to the HTML5 spec.
 
+If you need a lightweight and unobtrusive plugin to make your forms behave, this might be good for you.
+
+Test coverage
+-------------
+
+Test coverage is approx. 50%.
+
+Features
+--------
+
+HTML5FormValidation support for the following input types:
+
+* input:text
+* input:email
+* input:url
+* input:tel
+* input:checkbox
+* textarea
+* select
+
+Support for the following attributes is added.
+
+* required
+* pattern
+
 How to use
-==========
+----------
+
+To use the jQuery plugin, simply add the following code on page load:
+
+	$('form').html5form();
+
+Requirements
+------------
+
+jQuery is currently required.
+
 
 
---
+++ tests/ValidationSpec.js
@@ -0,0 +1,71 @@
+
+describe("FormField test", function() {
+  var formField;
+    var textFormField, emailFormField, telFormField
+  beforeEach(function() {
+    formField = new HTML5Form.FormField();
+    input = document.createElement('input')
+    input.value = ''
+  });
+  it("Abstract class test", function() {
+    expect(formField.validate()).not.toBeFalsy()
+    expect(formField.required).toBeFalsy()
+  });
+  it("text class validation", function() {
+    textFormField = new HTML5Form.TextFormField(input);
+    expect(textFormField.element).not.toBeUndefined();
+    expect(textFormField.validate()).not.toBeFalsy();
+    input.setAttribute('required','required')
+    expect(textFormField.validate()).toBeFalsy();
+    input.value = 'test@example.com'
+    expect(textFormField.validate()).not.toBeFalsy();
+
+  });
+  it("email class validation", function() {
+    emailFormField = new HTML5Form.EmailFormField(input);
+    expect(emailFormField.element).not.toBeUndefined();
+    expect(emailFormField.validate()).not.toBeFalsy();
+    input.value = 'test@example.com'
+    expect(emailFormField.validate()).not.toBeFalsy();
+    input.value = 'not Valid_example.com'
+    expect(emailFormField.validate()).toBeFalsy();
+    input.setAttribute('required','required')
+    expect(emailFormField.validate()).toBeFalsy();
+    input.value = ''
+    expect(emailFormField.validate()).toBeFalsy();
+  });
+  it("custom pattern validation", function() {
+    textFormField = new HTML5Form.TextFormField(input);
+    input.setAttribute('pattern','^[a-z]+$')
+    input.value = 'testfe@efcom'
+    expect(textFormField.validate()).toBeFalsy();
+    input.value = 'testfeefcom'
+    expect(textFormField.validate()).not.toBeFalsy();
+  })
+
+
+  describe("Select ", function() {
+    var formField, input, option
+    beforeEach(function() {
+      input = document.createElement('select')
+      formField = new HTML5Form.SelectFormField(input);
+      var option = document.createElement('option')
+      input.appendChild(option)
+      
+      option = document.createElement('option')
+      option.value = 'myValue'
+      input.appendChild(option)
+
+      var option = document.createElement('option')
+      option.value = 'myValue2'
+      input.appendChild(option)
+
+
+    });
+    it("Select test", function() {
+      expect(formField.validate()).not.toBeFalsy()
+      expect(formField.required).toBeFalsy()
+    });
+  });
+
+});
